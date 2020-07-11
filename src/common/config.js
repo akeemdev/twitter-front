1a1db7fc5dea7bec0e5d08e2b837702c1a898294
akeemdev <akeemdev87@gmail.com>
2020-07-11 12:08:34 -0400

    Initial

---
+++ src/common/config.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://realworldapi.techlord.net/api";
+export default API_URL;
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <div class="container">
+      <router-link class="logo-font" :to="{ name: 'home', params: {} }">
+        Twitter-Post
+      </router-link>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "RwvFooter"
+};
+</script>
